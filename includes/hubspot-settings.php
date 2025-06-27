327d0d805b9a8f301b51cf0e1d2ec026492642a4
John O'Connor <john@weblifter.com.au>
2025-06-27 17:04:52 +1000

    Fix HubSpot auth redirect

--- includes/hubspot-settings.php
+++ includes/hubspot-settings.php
@@ -114,21 +114,12 @@ class HubSpot_WC_Settings {
                 <li><strong><?php esc_html_e('Access Token:', 'hub-woo-sync'); ?></strong> <span id="access-token">...</span></li>
             </ul>
         </div>
-        <a href="#" data-auth-url="<?php echo esc_url($auth_url); ?>" data-nonce="<?php echo esc_attr($nonce); ?>" class="button-primary" id="hubspot-auth-button"><?php esc_html_e('Connect HubSpot', 'hub-woo-sync'); ?></a>
+        <a href="#" data-auth-url="<?php echo esc_url($auth_url); ?>" class="button-primary" id="hubspot-auth-button"><?php esc_html_e('Connect HubSpot', 'hub-woo-sync'); ?></a>
         <script>
         jQuery(function($){
             $('#hubspot-auth-button').on('click', function(e){
                 e.preventDefault();
-                const url = $(this).data('auth-url');
-                const nonce = $(this).data('nonce');
-                fetch(url, {
-                    credentials: 'include',
-                    headers: { 'X-WP-Nonce': nonce }
-                }).then(function(resp){
-                    if (resp.redirected) {
-                        window.location.href = resp.url;
-                    }
-                });
+                window.location.href = $(this).data('auth-url');
             });
 
             function checkHubSpotConnection() {
